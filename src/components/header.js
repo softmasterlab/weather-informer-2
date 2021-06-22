3d0c98d9213aed3fcccac96cd8011d8b1daad21a
Vladimir <45636169+softmasterlab@users.noreply.github.com>
2021-06-22 11:10:10 +0300

    add subheader h4 to header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,7 @@ export const Header = () => (
   <div className="header">
     <h2>Погодный информер</h2>
     <h3>Получение прогноза погоды по городам мира</h3>
+    <h4 style={{color: 'darkcyan'}}>(Автоматическое обновление версий)</h4>
     <hr />
   </div>
 );
